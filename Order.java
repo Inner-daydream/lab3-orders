2affc97af8c5d69001ecb14a9ab78a24f27a9623
Lucas <contact@lucasquitman.fr>
2023-06-07 15:37:22 -0400

    implementing the logic for the Order class

--- Order.java
+++ Order.java
@@ -1,21 +1,32 @@
+import java.util.concurrent.atomic.AtomicLong;
 public class Order {
-    private int id;
+    static final AtomicLong NEXT_ID = new AtomicLong(0);
+    final long id = NEXT_ID.getAndIncrement();
     private int itemCount;
     private double itemCost;
-    
+    private OrderManager manager;
+
+    Order(OrderManager manager) {
+        this.manager = manager;
+    }
+
     public int getCount() {
         return itemCount;
+    }
+    public  void applyDiscount(double discount) {
+        itemCost -= discount;
     }
     public double getTotalCost() {
         return itemCost;
     }
     public void addItem(double price) {
+        itemCount++;
+        itemCost += price;
+        manager.notifyObservers(this, EventType.ITEM_ADDED);
     }
+
     @Override
     public String toString() {
-        return "";
-    }
-    public void notifyObservers() {
-
+        return "Order [id=" + id + ", itemCount=" + itemCount + ", itemCost=" + itemCost + "]";
     }
 }
