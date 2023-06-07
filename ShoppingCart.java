476a0b5dcb85b6d3aab44b53e51fe90a50f7b9cf
Lucas <contact@lucasquitman.fr>
2023-06-07 15:38:13 -0400

    Adding some test code to our driver.

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,5 +1,24 @@
 public class ShoppingCart {
     public static void main(String[] args) {
+        OrderManager manager = new OrderManager();
+        QuantityMonitor quantityMonitor = new QuantityMonitor(5, 10);
+        PriceMonitor priceMonitor = new PriceMonitor(200, 20);
+        manager.attach(priceMonitor, EventType.ITEM_ADDED);
+        manager.attach(quantityMonitor, EventType.ITEM_ADDED);
+
+        Order order = new Order(manager);
+        order.addItem(100);
+        System.out.println(order);
+        order.addItem(101);
+        System.out.println(order);
+        for (int i = 0; i < 10; i++) {
+            order.addItem(10);
+        }
+        System.out.println(order);
+        Order secondOrder = new Order(manager);
+        secondOrder.addItem(199);
+        System.out.println(secondOrder);
+
 
     }
 }
